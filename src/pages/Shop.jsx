9758e92a263b337eb5bab11b40c19195d3666ea0
Chotipong5408 <chothipong5408@gmail.com>
2025-01-22 00:22:00 +0700

    deploy2

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from 'react'
-import ProductCard from '../components/card/ProductCard'
-import useEcomStore from '../store/ecom-store'
-import SearchCard from '../components/card/SearchCard'
+import React, { useEffect } from "react";
+import ProductCard from "../components/card/ProductCard";
+import useEcomStore from "../store/ecom-store";
+import SearchCard from "../components/card/SearchCard";
 
 const Shop = () => {
-  const getProduct = useEcomStore((state)=>state.getProduct)
-  const products = useEcomStore((state)=>state.products)
-  
-  useEffect(()=>{
-    getProduct()
-  },[])
+  const getProduct = useEcomStore((state) => state.getProduct);
+  const products = useEcomStore((state) => state.products);
+
+  useEffect(() => {
+    getProduct();
+  }, []);
 
   const style = {
     fontFamily: "'Sarabun', sans-serif", // กำหนดฟอนต์ที่ต้องการ
@@ -17,33 +17,23 @@ const Shop = () => {
 
   return (
     <div style={style}>
-    <div className='flex'>
+      <div className="flex">
+        {/* SearchBar */}
+        <div className="w-1/4 p-4 bg-gray-100 h-screen">
+          <SearchCard />
+        </div>
 
-      {/* SearchBar */}
-      <div className='w-1/4 p-4 bg-gray-100 h-screen'>
-        <SearchCard />
-      </div>
-
-      {/* Product */}
-      <div className='w-full p-4  overflow-y-auto'>
-        <p className='text-2xl font-bold mb-'></p>
-        <div className='flex flex-wrap gap-4'>
-          {/* Product Card */}
-          {
-            products.map((item,index)=>
-              <ProductCard key={index} item={item}/>
-            )
-          }
-         
-          {/* Product Card */}
+        {/* Product */}
+        <div className="w-full p-4 overflow-y-auto max-h-screen">
+          <div className="flex flex-wrap gap-4">
+            {products.map((item) => (
+              <ProductCard key={item.id} item={item} />
+            ))}
+          </div>
         </div>
       </div>
-
-
-
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
