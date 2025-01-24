d804762fe8074ba0417e108cbf031b08ce8dd744
chotipong <chothipong5408@gmail.com>
2025-01-24 13:57:01 +0700

    depoly2

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -50,7 +50,7 @@ function MainNav() {
                 to={"/"}
                 className="text-3xl font-bold bg-white flex items-center gap-2"
               >
-                <i className="bi bi-pc-display text-3xl"></i> COMDIY
+                <i className="bi bi-pc-display text-3xl"></i> COMD
               </Link>
 
               <NavLink
