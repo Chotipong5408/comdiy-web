97f306355c1763c044da0e130fd1f9e96ed6c5c0
Chotipong5408 <chothipong5408@gmail.com>
2025-02-01 02:23:30 +0700

    deploy2

--- src/utils/SwiperShowProduct.jsx
+++ src/utils/SwiperShowProduct.jsx
@@ -11,43 +11,46 @@ import { Pagination, Autoplay, Navigation } from "swiper/modules";
 
 const SwiperShowProduct = ({ children }) => {
   return (
-<Swiper
-  slidesPerView={5}
-  spaceBetween={10}
-  pagination={ false }
-  navigation={true}
-  modules={[Pagination, Autoplay, Navigation]}
-  autoplay={{
-    delay: 3000,
-    disableOnInteraction: false,
-  }}
-  breakpoints={{
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-    1280: {
-      slidesPerView: 6,
-      spaceBetween: 50,
-    },
-  }}
-  className="custom-swiper mySwiper object-cover rounded-md"
->
-  {children}
-</Swiper>
-
+    <Swiper
+      slidesPerView={2} // จำนวนสไลด์แสดงในมือถือ
+      spaceBetween={10} // ลดช่องว่างระหว่างสไลด์
+      pagination={false}
+      navigation={true}
+      modules={[Pagination, Autoplay, Navigation]}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        320: {
+          slidesPerView: 1, // สำหรับมือถือขนาดเล็กสุด
+          spaceBetween: 10,
+        },
+        480: {
+          slidesPerView: 2, // สำหรับมือถือขนาดกลาง
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 3, // สำหรับหน้าจอขนาดกลาง
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4, // สำหรับแท็บเล็ต
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 5, // สำหรับจอคอมพิวเตอร์
+          spaceBetween: 40,
+        },
+        1280: {
+          slidesPerView: 6, // สำหรับจอขนาดใหญ่
+          spaceBetween: 50,
+        },
+      }}
+      className="custom-swiper mySwiper object-cover rounded-md"
+    >
+      {children}
+    </Swiper>
   );
 };
 
